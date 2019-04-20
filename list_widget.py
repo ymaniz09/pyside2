b3efb86dca96a461a440f461c29729ce9845e126
ymaniz09 <ymaniz09@motorola.com>
2019-04-20 20:58:49 -0300

    Populating QListWidget

--- list_widget.py
+++ list_widget.py
@@ -6,7 +6,11 @@ if __name__ == '__main__':
 
     app = QApplication(sys.argv)
 
+    # Let's make the QListWidget show this data
+    data = ["ONE", "TWO", "THREE", "FOUR", "FIVE"]
+
     list_widget = QListWidget()
     list_widget.show()
+    list_widget.addItems(data)
 
     sys.exit(app.exec_())
